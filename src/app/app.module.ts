c752074b853ab5ee4b645b701b47be8ac63ff4ed
BeVali <vchiriac67@gmail.com>
2023-11-09 12:45:16 +0200

    Commiting changes before updating Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthComponent } from './auth/auth.component';
+import { AuthLoginComponent } from './auth/auth-login/auth-login.component';
+import { AuthSignupComponent } from './auth/auth-signup/auth-signup.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { SideNavigationComponent } from './home/side-navigation/side-navigation.component';
+import { MainNavigationComponent } from './home/main-navigation/main-navigation.component';
+import { MainPanelComponent } from './home/main-panel/main-panel.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLoginComponent,
+    AuthComponent,
+    AuthSignupComponent,
+    HomeComponent,
+    SideNavigationComponent,
+    MainNavigationComponent,
+    MainPanelComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth-signup/auth-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-signup',
+  templateUrl: './auth-signup.component.html',
+  styleUrls: ['./auth-signup.component.scss']
+})
+export class AuthSignupComponent {
+
+}
