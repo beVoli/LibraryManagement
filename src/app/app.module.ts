5aa3bef206d755bd8e63e2a065dab1965671ab21
beVoli <vchiriac68@yahoo.com>
2023-12-14 14:47:36 +0200

    Removed ng-bootstrap from angular.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +26,6 @@ import { MainPanelComponent } from './home/main-panel/main-panel.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     AppRoutingModule,
     FormsModule
   ],
